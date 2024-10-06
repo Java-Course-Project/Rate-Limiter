e5876e3d1090c895124e798ff97d6c66b88642ab
duyvk <imkhuongduy@gmail.com>
2024-10-06 11:47:02 +0700

    add comment for reason using lua script

--- src/main/java/org/example/ratelimiter/service/RateLimiterService.java
+++ src/main/java/org/example/ratelimiter/service/RateLimiterService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.ratelimiter.config.RateLimiterProperties;
 import org.example.ratelimiter.model.TokenBucket;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,7 @@ public class RateLimiterService {
 	private final RateLimiterProperties rateLimiterProperties;
 	private final RedisTemplate<String, TokenBucket> redisTemplate;
 	private static final String RATE_LIMITER_KEY = "rate-limiter";
+	private final Resource luaScriptResource = new ClassPathResource("lua/check_and_set_rate_limiter.lua");
 
 	public boolean tryAcquire(int token) {
 		List<String> keys = List.of(RATE_LIMITER_KEY);
@@ -25,9 +27,10 @@ public class RateLimiterService {
 									rateLimiterProperties.getRefillRate().toMillis(), token,
 									rateLimiterProperties.getBucketCapacity(), rateLimiterProperties.getBucketSize());
 
-
-		return Boolean.TRUE.equals(
-				redisTemplate.execute(RedisScript.of(new ClassPathResource("lua/check_and_set_rate_limiter.lua"), Boolean.class), keys,
-									  args.toArray(new Object[0])));
+		// As there could be many instances access at a same time, checking request process must be atomic. Transaction can't be used as
+		// the process include get and then check process and get inside a transaction would return null for all cases. Therefore, lua
+		// script or distributed lock (SETNX) must be used. In this case, we chose lua script.
+		return Boolean.TRUE.equals(redisTemplate.execute(RedisScript.of(luaScriptResource, Boolean.class), keys,
+														 args.toArray(new Object[0])));
 	}
 }
